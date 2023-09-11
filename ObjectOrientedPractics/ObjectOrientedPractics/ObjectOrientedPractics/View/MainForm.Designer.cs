825ee4880b0ac37f25e02bfa51bde44ce4d702f1
Ingenious01 <tishenkoanton26@gmail.com>
2023-09-11 18:26:33 +0700

    Front End is done a lot

--- ObjectOrientedPractics/ObjectOrientedPractics/ObjectOrientedPractics/View/MainForm.Designer.cs
+++ ObjectOrientedPractics/ObjectOrientedPractics/ObjectOrientedPractics/View/MainForm.Designer.cs
@@ -30,52 +30,77 @@
         {
             this.MainTavPage = new System.Windows.Forms.TabControl();
             this.ItemsTabPage = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.itemsTab1 = new ObjectOrientedPractics.View.Tabs.ItemsTab();
+            this.CustomersTabPage = new System.Windows.Forms.TabPage();
+            this.customerTab1 = new ObjectOrientedPractics.View.Tabs.CustomerTab();
             this.MainTavPage.SuspendLayout();
+            this.ItemsTabPage.SuspendLayout();
+            this.CustomersTabPage.SuspendLayout();
             this.SuspendLayout();
             // 
             // MainTavPage
             // 
             this.MainTavPage.Controls.Add(this.ItemsTabPage);
-            this.MainTavPage.Controls.Add(this.tabPage2);
+            this.MainTavPage.Controls.Add(this.CustomersTabPage);
             this.MainTavPage.Dock = System.Windows.Forms.DockStyle.Fill;
             this.MainTavPage.Location = new System.Drawing.Point(0, 0);
             this.MainTavPage.Name = "MainTavPage";
             this.MainTavPage.SelectedIndex = 0;
-            this.MainTavPage.Size = new System.Drawing.Size(927, 631);
+            this.MainTavPage.Size = new System.Drawing.Size(996, 762);
             this.MainTavPage.TabIndex = 0;
             // 
             // ItemsTabPage
             // 
+            this.ItemsTabPage.Controls.Add(this.itemsTab1);
             this.ItemsTabPage.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.ItemsTabPage.Location = new System.Drawing.Point(4, 25);
             this.ItemsTabPage.Name = "ItemsTabPage";
             this.ItemsTabPage.Padding = new System.Windows.Forms.Padding(3);
-            this.ItemsTabPage.Size = new System.Drawing.Size(919, 602);
+            this.ItemsTabPage.Size = new System.Drawing.Size(988, 733);
             this.ItemsTabPage.TabIndex = 0;
-            this.ItemsTabPage.Text = "Item";
+            this.ItemsTabPage.Text = "Items";
             this.ItemsTabPage.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // itemsTab1
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 25);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 71);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.itemsTab1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.itemsTab1.Location = new System.Drawing.Point(3, 3);
+            this.itemsTab1.Margin = new System.Windows.Forms.Padding(4);
+            this.itemsTab1.Name = "itemsTab1";
+            this.itemsTab1.Size = new System.Drawing.Size(982, 727);
+            this.itemsTab1.TabIndex = 0;
+            // 
+            // CustomersTabPage
+            // 
+            this.CustomersTabPage.Controls.Add(this.customerTab1);
+            this.CustomersTabPage.Location = new System.Drawing.Point(4, 25);
+            this.CustomersTabPage.Name = "CustomersTabPage";
+            this.CustomersTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.CustomersTabPage.Size = new System.Drawing.Size(988, 733);
+            this.CustomersTabPage.TabIndex = 1;
+            this.CustomersTabPage.Text = "Customers";
+            this.CustomersTabPage.UseVisualStyleBackColor = true;
+            // 
+            // customerTab1
+            // 
+            this.customerTab1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.customerTab1.Location = new System.Drawing.Point(3, 3);
+            this.customerTab1.Name = "customerTab1";
+            this.customerTab1.Size = new System.Drawing.Size(982, 727);
+            this.customerTab1.TabIndex = 0;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(927, 631);
+            this.ClientSize = new System.Drawing.Size(996, 762);
             this.Controls.Add(this.MainTavPage);
             this.Name = "MainForm";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.MainTavPage.ResumeLayout(false);
+            this.ItemsTabPage.ResumeLayout(false);
+            this.CustomersTabPage.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -84,7 +109,9 @@
 
         private System.Windows.Forms.TabControl MainTavPage;
         private System.Windows.Forms.TabPage ItemsTabPage;
-        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage CustomersTabPage;
+        private View.Tabs.ItemsTab itemsTab1;
+        private View.Tabs.CustomerTab customerTab1;
     }
 }
 
--- ObjectOrientedPractics/ObjectOrientedPractics/ObjectOrientedPractics/View/Tabs/CustomerTab.Designer.cs
+++ ObjectOrientedPractics/ObjectOrientedPractics/ObjectOrientedPractics/View/Tabs/CustomerTab.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ObjectOrientedPractics.View.Tabs
 {
-    partial class ItemsTab
+    partial class CustomerTab
     {
         /// <summary> 
         /// Обязательная переменная конструктора.
@@ -30,23 +30,21 @@
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            this.ItemsGroupBox = new System.Windows.Forms.GroupBox();
-            this.ItemsListBox = new System.Windows.Forms.ListBox();
+            this.CustomersGroupBox = new System.Windows.Forms.GroupBox();
+            this.CustomersListBox = new System.Windows.Forms.ListBox();
             this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
             this.AddButton = new System.Windows.Forms.Button();
             this.RemoveButton = new System.Windows.Forms.Button();
             this.SelectedItemGroupBox = new System.Windows.Forms.GroupBox();
-            this.DescriptionRichTextBox = new System.Windows.Forms.RichTextBox();
-            this.SearchDescriptionLabel = new System.Windows.Forms.Label();
             this.NameRichTextBox = new System.Windows.Forms.RichTextBox();
-            this.SearchNameLabel = new System.Windows.Forms.Label();
+            this.SearchAdressLabel = new System.Windows.Forms.Label();
             this.CostTextBox = new System.Windows.Forms.TextBox();
             this.IdTextBox = new System.Windows.Forms.TextBox();
-            this.CostLabel = new System.Windows.Forms.Label();
+            this.FullNameLabel = new System.Windows.Forms.Label();
             this.IdLabel = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
-            this.ItemsGroupBox.SuspendLayout();
+            this.CustomersGroupBox.SuspendLayout();
             this.tableLayoutPanel3.SuspendLayout();
             this.SelectedItemGroupBox.SuspendLayout();
             this.SuspendLayout();
@@ -63,14 +61,14 @@
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(997, 691);
-            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1072, 881);
+            this.tableLayoutPanel1.TabIndex = 1;
             // 
             // tableLayoutPanel2
             // 
             this.tableLayoutPanel2.ColumnCount = 1;
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.Controls.Add(this.ItemsGroupBox, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.CustomersGroupBox, 0, 0);
             this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 0, 1);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
@@ -78,30 +76,30 @@
             this.tableLayoutPanel2.RowCount = 2;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(404, 685);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(435, 875);
             this.tableLayoutPanel2.TabIndex = 0;
             // 
-            // ItemsGroupBox
+            // CustomersGroupBox
             // 
-            this.ItemsGroupBox.Controls.Add(this.ItemsListBox);
-            this.ItemsGroupBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.ItemsGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.ItemsGroupBox.Location = new System.Drawing.Point(3, 3);
-            this.ItemsGroupBox.Name = "ItemsGroupBox";
-            this.ItemsGroupBox.Size = new System.Drawing.Size(398, 610);
-            this.ItemsGroupBox.TabIndex = 0;
-            this.ItemsGroupBox.TabStop = false;
-            this.ItemsGroupBox.Text = "Items";
+            this.CustomersGroupBox.Controls.Add(this.CustomersListBox);
+            this.CustomersGroupBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.CustomersGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CustomersGroupBox.Location = new System.Drawing.Point(3, 3);
+            this.CustomersGroupBox.Name = "CustomersGroupBox";
+            this.CustomersGroupBox.Size = new System.Drawing.Size(429, 781);
+            this.CustomersGroupBox.TabIndex = 0;
+            this.CustomersGroupBox.TabStop = false;
+            this.CustomersGroupBox.Text = "Customers";
             // 
-            // ItemsListBox
+            // CustomersListBox
             // 
-            this.ItemsListBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.ItemsListBox.FormattingEnabled = true;
-            this.ItemsListBox.ItemHeight = 26;
-            this.ItemsListBox.Location = new System.Drawing.Point(3, 28);
-            this.ItemsListBox.Name = "ItemsListBox";
-            this.ItemsListBox.Size = new System.Drawing.Size(392, 579);
-            this.ItemsListBox.TabIndex = 0;
+            this.CustomersListBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.CustomersListBox.FormattingEnabled = true;
+            this.CustomersListBox.ItemHeight = 26;
+            this.CustomersListBox.Location = new System.Drawing.Point(3, 28);
+            this.CustomersListBox.Name = "CustomersListBox";
+            this.CustomersListBox.Size = new System.Drawing.Size(423, 750);
+            this.CustomersListBox.TabIndex = 0;
             // 
             // tableLayoutPanel3
             // 
@@ -112,11 +110,13 @@
             this.tableLayoutPanel3.Controls.Add(this.AddButton, 0, 0);
             this.tableLayoutPanel3.Controls.Add(this.RemoveButton, 1, 0);
             this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 619);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 790);
             this.tableLayoutPanel3.Name = "tableLayoutPanel3";
             this.tableLayoutPanel3.RowCount = 1;
             this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel3.Size = new System.Drawing.Size(398, 63);
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 82F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 82F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(429, 82);
             this.tableLayoutPanel3.TabIndex = 1;
             // 
             // AddButton
@@ -125,7 +125,7 @@
             this.AddButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.AddButton.Location = new System.Drawing.Point(3, 3);
             this.AddButton.Name = "AddButton";
-            this.AddButton.Size = new System.Drawing.Size(126, 57);
+            this.AddButton.Size = new System.Drawing.Size(137, 76);
             this.AddButton.TabIndex = 0;
             this.AddButton.Text = "Add";
             this.AddButton.UseVisualStyleBackColor = true;
@@ -134,100 +134,80 @@
             // 
             this.RemoveButton.Dock = System.Windows.Forms.DockStyle.Fill;
             this.RemoveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.RemoveButton.Location = new System.Drawing.Point(135, 3);
+            this.RemoveButton.Location = new System.Drawing.Point(146, 3);
             this.RemoveButton.Name = "RemoveButton";
-            this.RemoveButton.Size = new System.Drawing.Size(126, 57);
+            this.RemoveButton.Size = new System.Drawing.Size(137, 76);
             this.RemoveButton.TabIndex = 1;
             this.RemoveButton.Text = "Remove";
             this.RemoveButton.UseVisualStyleBackColor = true;
             // 
             // SelectedItemGroupBox
             // 
-            this.SelectedItemGroupBox.Controls.Add(this.DescriptionRichTextBox);
-            this.SelectedItemGroupBox.Controls.Add(this.SearchDescriptionLabel);
             this.SelectedItemGroupBox.Controls.Add(this.NameRichTextBox);
-            this.SelectedItemGroupBox.Controls.Add(this.SearchNameLabel);
+            this.SelectedItemGroupBox.Controls.Add(this.SearchAdressLabel);
             this.SelectedItemGroupBox.Controls.Add(this.CostTextBox);
             this.SelectedItemGroupBox.Controls.Add(this.IdTextBox);
-            this.SelectedItemGroupBox.Controls.Add(this.CostLabel);
+            this.SelectedItemGroupBox.Controls.Add(this.FullNameLabel);
             this.SelectedItemGroupBox.Controls.Add(this.IdLabel);
             this.SelectedItemGroupBox.Dock = System.Windows.Forms.DockStyle.Fill;
             this.SelectedItemGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.SelectedItemGroupBox.Location = new System.Drawing.Point(413, 3);
+            this.SelectedItemGroupBox.Location = new System.Drawing.Point(444, 3);
             this.SelectedItemGroupBox.Name = "SelectedItemGroupBox";
-            this.SelectedItemGroupBox.Size = new System.Drawing.Size(581, 685);
+            this.SelectedItemGroupBox.Size = new System.Drawing.Size(625, 875);
             this.SelectedItemGroupBox.TabIndex = 1;
             this.SelectedItemGroupBox.TabStop = false;
-            this.SelectedItemGroupBox.Text = "Selected Item";
-            // 
-            // DescriptionRichTextBox
-            // 
-            this.DescriptionRichTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.DescriptionRichTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.DescriptionRichTextBox.Location = new System.Drawing.Point(11, 377);
-            this.DescriptionRichTextBox.Name = "DescriptionRichTextBox";
-            this.DescriptionRichTextBox.Size = new System.Drawing.Size(565, 236);
-            this.DescriptionRichTextBox.TabIndex = 7;
-            this.DescriptionRichTextBox.Text = "";
-            this.DescriptionRichTextBox.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
-            // 
-            // SearchDescriptionLabel
-            // 
-            this.SearchDescriptionLabel.AutoSize = true;
-            this.SearchDescriptionLabel.Location = new System.Drawing.Point(7, 348);
-            this.SearchDescriptionLabel.Name = "SearchDescriptionLabel";
-            this.SearchDescriptionLabel.Size = new System.Drawing.Size(127, 26);
-            this.SearchDescriptionLabel.TabIndex = 6;
-            this.SearchDescriptionLabel.Text = "Description:";
-            this.SearchDescriptionLabel.Click += new System.EventHandler(this.label1_Click);
+            this.SelectedItemGroupBox.Text = "Selected Customer";
             // 
             // NameRichTextBox
             // 
-            this.NameRichTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.NameRichTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.NameRichTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.NameRichTextBox.Location = new System.Drawing.Point(10, 186);
+            this.NameRichTextBox.Location = new System.Drawing.Point(111, 124);
             this.NameRichTextBox.Name = "NameRichTextBox";
-            this.NameRichTextBox.Size = new System.Drawing.Size(565, 140);
+            this.NameRichTextBox.Size = new System.Drawing.Size(469, 599);
             this.NameRichTextBox.TabIndex = 5;
             this.NameRichTextBox.Text = "";
             // 
-            // SearchNameLabel
+            // SearchAdressLabel
             // 
-            this.SearchNameLabel.AutoSize = true;
-            this.SearchNameLabel.Location = new System.Drawing.Point(6, 157);
-            this.SearchNameLabel.Name = "SearchNameLabel";
-            this.SearchNameLabel.Size = new System.Drawing.Size(77, 26);
-            this.SearchNameLabel.TabIndex = 4;
-            this.SearchNameLabel.Text = "Name:";
+            this.SearchAdressLabel.AutoSize = true;
+            this.SearchAdressLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SearchAdressLabel.Location = new System.Drawing.Point(6, 124);
+            this.SearchAdressLabel.Name = "SearchAdressLabel";
+            this.SearchAdressLabel.Size = new System.Drawing.Size(74, 24);
+            this.SearchAdressLabel.TabIndex = 4;
+            this.SearchAdressLabel.Text = "Adress:";
             // 
             // CostTextBox
             // 
+            this.CostTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.CostTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.CostTextBox.Location = new System.Drawing.Point(67, 82);
+            this.CostTextBox.Location = new System.Drawing.Point(111, 83);
             this.CostTextBox.Name = "CostTextBox";
-            this.CostTextBox.Size = new System.Drawing.Size(141, 28);
+            this.CostTextBox.Size = new System.Drawing.Size(469, 28);
             this.CostTextBox.TabIndex = 3;
+            this.CostTextBox.TextChanged += new System.EventHandler(this.CostTextBox_TextChanged);
             // 
             // IdTextBox
             // 
             this.IdTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.IdTextBox.Location = new System.Drawing.Point(67, 44);
+            this.IdTextBox.Location = new System.Drawing.Point(111, 44);
             this.IdTextBox.Name = "IdTextBox";
-            this.IdTextBox.Size = new System.Drawing.Size(141, 28);
+            this.IdTextBox.Size = new System.Drawing.Size(151, 28);
             this.IdTextBox.TabIndex = 2;
             // 
-            // CostLabel
+            // FullNameLabel
             // 
-            this.CostLabel.AutoSize = true;
-            this.CostLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.CostLabel.Location = new System.Drawing.Point(6, 85);
-            this.CostLabel.Name = "CostLabel";
-            this.CostLabel.Size = new System.Drawing.Size(52, 24);
-            this.CostLabel.TabIndex = 1;
-            this.CostLabel.Text = "Cost:";
+            this.FullNameLabel.AutoSize = true;
+            this.FullNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FullNameLabel.Location = new System.Drawing.Point(6, 87);
+            this.FullNameLabel.Name = "FullNameLabel";
+            this.FullNameLabel.Size = new System.Drawing.Size(99, 24);
+            this.FullNameLabel.TabIndex = 1;
+            this.FullNameLabel.Text = "Full name:";
             // 
             // IdLabel
             // 
@@ -239,16 +219,16 @@
             this.IdLabel.TabIndex = 0;
             this.IdLabel.Text = "ID:";
             // 
-            // ItemsTab
+            // CustomerTab
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.tableLayoutPanel1);
-            this.Name = "ItemsTab";
-            this.Size = new System.Drawing.Size(997, 691);
+            this.Name = "CustomerTab";
+            this.Size = new System.Drawing.Size(1072, 881);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel2.ResumeLayout(false);
-            this.ItemsGroupBox.ResumeLayout(false);
+            this.CustomersGroupBox.ResumeLayout(false);
             this.tableLayoutPanel3.ResumeLayout(false);
             this.SelectedItemGroupBox.ResumeLayout(false);
             this.SelectedItemGroupBox.PerformLayout();
@@ -260,19 +240,17 @@
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
-        private System.Windows.Forms.GroupBox ItemsGroupBox;
-        private System.Windows.Forms.ListBox ItemsListBox;
+        private System.Windows.Forms.GroupBox CustomersGroupBox;
+        private System.Windows.Forms.ListBox CustomersListBox;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
         private System.Windows.Forms.Button AddButton;
         private System.Windows.Forms.Button RemoveButton;
         private System.Windows.Forms.GroupBox SelectedItemGroupBox;
-        private System.Windows.Forms.RichTextBox DescriptionRichTextBox;
-        private System.Windows.Forms.Label SearchDescriptionLabel;
         private System.Windows.Forms.RichTextBox NameRichTextBox;
-        private System.Windows.Forms.Label SearchNameLabel;
+        private System.Windows.Forms.Label SearchAdressLabel;
         private System.Windows.Forms.TextBox CostTextBox;
         private System.Windows.Forms.TextBox IdTextBox;
-        private System.Windows.Forms.Label CostLabel;
+        private System.Windows.Forms.Label FullNameLabel;
         private System.Windows.Forms.Label IdLabel;
     }
 }
